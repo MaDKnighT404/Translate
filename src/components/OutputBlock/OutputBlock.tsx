f7d762ab869fef21e5ea70d9f76303ca8b6421b5
George Koloidi <Battleaxe@bk.ru>
2023-03-06 21:10:39 +0100

    init: start translate project

---
+++ src/components/OutputBlock/OutputBlock.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OutputBlock = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default OutputBlock;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import InputBlock from '../components/InputBlock/InputBlock';
+import OutputBlock from '../components/OutputBlock/OutputBlock';
+
+const MainPage = () => {
+  return (
+    <>
+     <InputBlock /> 
+     <OutputBlock /> 
+    </>
+  );
+};
+
+export default MainPage;
